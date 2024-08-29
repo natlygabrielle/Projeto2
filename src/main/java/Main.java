5b9bbdff7748f16c852df289152cbd64805b6cd9
20221080030240 <natlygabrielle2000@gmail.com>
2024-08-29 12:13:42 -0300

    Natly.commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,22 @@
 import Model.Pagamento;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory =Persistence.createEntityManagerFactory("pagamentosPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
         Pagamento pg = new Pagamento();
-        pg.setId(11);
         pg.setValor(500);
         pg.setDate(new Date());
         System.out.println(pg);
+        entityManager.persist(pg);
+        entityManager.getTransaction().commit();
+        entityManagerFactory.close();
+        entityManager.close();
     }
 }
--- src/main/java/Model/Pagamento.java
+++ src/main/java/Model/Pagamento.java
@@ -1,10 +1,18 @@
 package Model;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table(name = "tb_pagamento")
 public class Pagamento {
+  @Id
+  @GeneratedValue( strategy = GenerationType.IDENTITY)
     private long id;
+  @Column(name = "data_pagamento")
     private Date data;
+  @Column(name = "valor_pagamento")
     private float valor;
 
     @Override
